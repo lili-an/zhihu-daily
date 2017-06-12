dfb69f1a4adf30c06632ce4637e1fb11c69ca024
strong <70786564@qq.com>
2017-06-12 13:50:13 +0800

    'build_dist'

--- src/components/pop.vue
+++
@@ -1,9 +0,0 @@
-<template lang="html">
-
-</template>
-<script type="text/javascript">
-	
-</script>
-<style type="text/css" scoped>
-	
-</style>
--- src/store/types.js
+++
@@ -1,5 +0,0 @@
-const INCREMENT = "INCREMENT";
-const CHANGE_ACTIVE = "CHANGE_ACTIVE";
-const SET_MOVIE_LIST = "SET_MOVIE_LIST";
-const SET_FLAG = "SET_FLAG";
-const SET_BottomPopup = "SET_BottomPopup";
